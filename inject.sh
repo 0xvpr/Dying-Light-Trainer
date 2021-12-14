6dd5d2c2138e4bb99b3920e3f8588102f2fdfc8f
0xvpr <malikrb@live.com>
2021-12-14 12:18:34 -0600

    improved inject.sh

--- inject.sh
+++ inject.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Creator: VPR
+# Created: December 14th, 2021
+# Updated: December 14th, 2021
+
+# This script is intended to work with Pidjeon for a seamless injection process.
+
 __game_name="DyingLightGame.exe"
 __dll_path="./bin/daylight-savings.dll"
+__pidjeon_path="~/.toolkit" # Change as necessary
 
-export PATH=$PATH:~/.toolkit/
+export PATH=$PATH:"${__pidjeon_path}"
 
 pidjeon_x64.exe "${__game_name}" "${__dll_path}" -i ManualMap
